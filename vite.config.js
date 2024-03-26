763c3c0c6a82f8936abbfad09741579da1dd2e22
etimvr <etimur@protonmail.com>
2024-03-26 15:27:46 +0500

    change base url, add CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
 })
-
-// TODO remove base field when moved to main domain
